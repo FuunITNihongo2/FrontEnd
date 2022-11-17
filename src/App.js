ad7903b987c34c56458f44e3ea4c404ed2f10430
Nhattruong2308 <nhattruong230801@gmail.com>
2022-11-17 12:38:38 +0700

    update Booth Details UI

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import ScrollToTop from "./ScrollToTop";
 import Login from "./page/Login";
 import Home from "./page/Home";
 import "swiper/css/bundle";
+import Layout from "./layout";
+import BoothDetail from "./page/BoothDetail";
 
 function App() {
   return (
     <Box minH={"100vh"} w="100vw" bg="#ffffff">
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="booth-details" element={<BoothDetail />} />
+        </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </Box>
